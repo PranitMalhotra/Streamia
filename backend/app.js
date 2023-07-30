01a4c9d4d993692690606047b9d2a91d27d9e3c8
PranitMalhotra <pranit.malhotra2021@vitstudent.ac.in>
2023-07-30 19:05:47 +0530

    added more comments

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
+// packages required for our project
 const express = require('express');
+
+//fs is used to read and writes from files
 const fs = require('fs');
 const cors = require('cors');
+
+//path finds a way to work with paths in files and directories
 const path = require('path');
 const app = express();
 
@@ -13,7 +18,7 @@ const Videos = require('./routes/videos')
 app.use('/videos', Videos)
 
 
-
+// we listen to our service at port 5000
 app.listen(5000, () => {
     console.log('Listening on port 5000!')
 });
--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -4,52 +4,57 @@ const videos = require('../mockData')
 const fs = require('fs')
 
 
-// get list of videos
+// get list of videos, to populate the homepage
 router.get('/', (req, res) => {
     res.json(videos)
 })
 
 
-// make request for a particular video
-router.get('/:id/data', (req,res)=> {
-    const id = parseInt(req.params.id, 10)
+// returns metadata for a single video
+router.get('/:id/data', (req, res) => {
+    const id = parseInt(req.params.id, 10) // parses the string into a integer with a radix of 10
     res.json(videos[id])
 })
 
-
+// used to stream a video of a given id
 router.get('/video/:id', (req, res) => {
-    const videoPath = `assets/${req.params.id}.mp4`;
+    const videoPath = `assets/${req.params.id}.mp4`; //we get the id of the video requested using the 'req.params.id'
     const videoStat = fs.statSync(videoPath);
-    const fileSize = videoStat.size;
-    const videoRange = req.headers.range;
+    const fileSize = videoStat.size; //reading the file size
+    const videoRange = req.headers.range; // range function lets the server know which chunk of the video to send back to the client
+    
+    
     if (videoRange) {
         const parts = videoRange.replace(/bytes=/, "").split("-");
         const start = parseInt(parts[0], 10);
         const end = parts[1]
-            ? parseInt(parts[1], 10)
-            : fileSize-1;
-        const chunksize = (end-start) + 1;
-        const file = fs.createReadStream(videoPath, {start, end});
+            ? parseInt(parts[1], 10) // implemented back pressure i.e I/O source is paused until the client is ready for more data
+            : fileSize - 1;
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(videoPath, { start, end }); // creates a read stream using the start and end values of the range
         const head = {
             'Content-Range': `bytes ${start}-${end}/${fileSize}`,
             'Accept-Ranges': 'bytes',
             'Content-Length': chunksize,
             'Content-Type': 'video/mp4',
         };
-        res.writeHead(206, head);
+        res.writeHead(206, head); // HTTP 206 used to signify that the content being served is partial instead of HTTP 200
         file.pipe(res);
+
+
+    // deals with browsers that don't send an range in the initial request
     } else {
         const head = {
-            'Content-Length': fileSize,
+            'Content-Length': fileSize, // gets the file size
             'Content-Type': 'video/mp4',
         };
         res.writeHead(200, head);
-        fs.createReadStream(videoPath).pipe(res);
+        fs.createReadStream(videoPath).pipe(res); 
     }
 });
 
 
 // captions route
-const captionPath = '/home/madfinger/Desktop/MyProjects/streaming-app/backend'
+const captionPath = '/home/prntmlhtr/Streamia/backend/'
 router.get('/video/:id/caption', (req, res) => res.sendFile(`assets/captions/${req.params.id}.vtt`, { root: captionPath }));
 module.exports = router;
